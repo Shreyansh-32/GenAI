61263172465d1fb03d98d946c352fa5f0aefd437
Shreyansh-32 <thakurshreyansh0609@gmail.com>
2026-01-07 23:18:25 +0530

    String basics

---
+++ 02_datatypes/chapter_06.py
@@ -0,0 +1,13 @@
+# String
+
+customer_name = "Shreyansh Thakur"
+chai_name = "Ginger chai"
+
+print(f"Order for {customer_name} : {chai_name} please!")
+
+print(f"First word : {customer_name.split(" ")[0]}")
+print(f"Last word : {customer_name.split(" ")[1]}")
+
+print(f"First word : {chai_name[:6]}")
+print(f"Last word : {chai_name[7:]}")
+print(f"Reverse string : {customer_name[::-1]}")
