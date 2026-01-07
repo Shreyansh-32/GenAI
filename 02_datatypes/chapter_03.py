9691a0ae9803f8c71803d1723438be592f708317
Shreyansh-32 <thakurshreyansh0609@gmail.com>
2026-01-07 22:43:16 +0530

    Integer

---
+++ 02_datatypes/chapter_03.py
@@ -0,0 +1,12 @@
+# Integer
+
+black_tea_grams = 14
+ginger_grams = 3
+
+print(f"Amount of tea : {black_tea_grams+ginger_grams}")
+print(f"Remaining tea : {black_tea_grams-ginger_grams}")
+
+tea_bags = 7
+servings = 4
+print(f"Tea bags per serving : {tea_bags/servings}")
+print(f"Whole tea bags per servings : {tea_bags//servings}")
