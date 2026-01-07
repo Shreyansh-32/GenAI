2efedcd7eaeab314076b318ddd3b8a15b14d867d
Shreyansh-32 <thakurshreyansh0609@gmail.com>
2026-01-07 22:46:30 +0530

    Boolean

---
+++ 02_datatypes/chapter_04.py
@@ -0,0 +1,9 @@
+# Boolean
+
+isBoiling = True
+tea_leaves_grams = 4
+isHot = False
+
+print(f"Total : {isBoiling+tea_leaves_grams}")
+print(f"Is tea leaves left? : {bool(tea_leaves_grams)}")
+print(f"And operation : {isBoiling and isHot}")
