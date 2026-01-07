f9dbfb83a9c90b5a1c81dd253af8ec5d8aab8f37
Shreyansh-32 <thakurshreyansh0609@gmail.com>
2026-01-07 22:50:16 +0530

    Real number

---
+++ 02_datatypes/chapter_05.py
@@ -0,0 +1,8 @@
+# Real -> floating
+
+ideal_temp = 95.5
+current_temp = 94.999999999
+
+print(f"Ideal temp : {ideal_temp}")
+print(f"Current temp : {current_temp}")
+print(f"Difference in temp : {ideal_temp-current_temp}")
