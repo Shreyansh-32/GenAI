84efa045aec6cee683421ffb9425e163d9c7738c
Shreyansh-32 <thakurshreyansh0609@gmail.com>
2026-02-15 09:48:57 +0530

    Imports in python

---
+++ 06_imports/main.py
@@ -0,0 +1,11 @@
+# import recepies.flavors
+
+# print(recepies.flavors.ginger_chai())
+
+from recepies.flavors import ginger_chai
+
+print(ginger_chai())
+
+from recepies.flavors import masala_chai as masala
+
+print(masala())
---
+++ 06_imports/recepies/flavors.py
@@ -0,0 +1,5 @@
+def ginger_chai():
+    return "Your ginger chai is raedy!"
+
+def masala_chai():
+    return "Your masala chai is ready!"
